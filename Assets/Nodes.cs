612d0b74ab33d4bffa6b27f759d5d0bf2c0a0663
Jacques <s-jacques@live.com>
2018-02-21 22:04:17 -0500

    Started project

---
+++ Assets/Nodes.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Nodes : MonoBehaviour
+{
+
+    public float rayThickness;
+    public Material rayMat;
+
+    private Dictionary<float, GameObject> neighbors;
+
+    void Start()
+    {
+        neighbors = new Dictionary<float, GameObject>();
+
+        foreach (GameObject node in GameObject.FindGameObjectsWithTag("node"))
+        {
+            RaycastHit hit;
+            Vector3 direction = node.transform.position - transform.position;
+
+            if (Physics.SphereCast(transform.position, rayThickness, direction, out hit) && hit.collider.gameObject.tag == "node")
+            {
+                try
+                {
+                    neighbors.Add(hit.distance, hit.collider.gameObject);
+                }
+                catch (ArgumentException e)
+                {
+                    continue;
+                }
+            }
+        }
+
+        foreach (KeyValuePair<float, GameObject> neighbor in neighbors)
+        {
+            GameObject line = new GameObject();
+            line.AddComponent<LineRenderer>();
+            LineRenderer lineRenderer = line.GetComponent<LineRenderer>();
+            lineRenderer.material = rayMat;
+            lineRenderer.startWidth = rayThickness;
+            lineRenderer.endWidth = rayThickness;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, neighbor.Value.transform.position);
+        }
+    }
+}
